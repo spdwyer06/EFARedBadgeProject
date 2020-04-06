714bd88fe77a36caf9c088275b29780b6ac06253
Sean Dwyergmail.com <spdwyer06@gmail.com>
2020-04-06 17:24:50 -0400

    Updated all game views to include price and changed GameName to GameTitle

--- GameStash/Controllers/GameController.cs
+++ GameStash/Controllers/GameController.cs
@@ -46,7 +46,7 @@ namespace GameStash.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "GameID,PlatformType,CategoryType,RatingType,GameName")] Game game)
+        public ActionResult Create([Bind(Include = "GameID,PlatformType,CategoryType,RatingType,GameTitle,Price")] Game game)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace GameStash.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "GameID,PlatformType,CategoryType,RatingType,GameName")] Game game)
+        public ActionResult Edit([Bind(Include = "GameID,PlatformType,CategoryType,RatingType,GameTitle,Price")] Game game)
         {
             if (ModelState.IsValid)
             {

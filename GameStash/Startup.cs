8b1c3ff8878fdbc042dbec9f0a5040bb8769eee8
Sean Dwyergmail.com <spdwyer06@gmail.com>
2020-04-06 14:09:38 -0400

    Add project files.

---
+++ GameStash/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameStash.Startup))]
+namespace GameStash
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

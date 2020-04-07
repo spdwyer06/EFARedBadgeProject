b8a05e29a94538df79fb338f1be14afe526ed186
Sean Dwyergmail.com <spdwyer06@gmail.com>
2020-04-07 10:09:03 -0400

    Filled out Game Create

--- GS_Models/Game/GameCreate.cs
+++ GS_Models/Game/GameCreate.cs
@@ -8,6 +8,14 @@ namespace GS_Models.Game
 {
     public class GameCreate
     {
+        public enum TypeOfPlatform { Playstation4, XboxOne, NintendoSwitch, PC }
+        public enum TypeOfCategory { FPS, RPG, Sports }
+        public enum TypeOfRating { E, T, M }
 
+        public string GameTitle { get; set; }
+        public TypeOfPlatform PlatformType { get; set; }
+        public TypeOfCategory CategoryType { get; set; }
+        public TypeOfRating RatingType { get; set; }
+        public double Price { get; set; }
     }
 }

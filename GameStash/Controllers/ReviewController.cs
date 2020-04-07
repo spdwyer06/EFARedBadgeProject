b1622ab9ef4be65d88f83ff02397de23b9895e8c
Sean Dwyergmail.com <spdwyer06@gmail.com>
2020-04-07 15:40:56 -0400

    Added base setup for Review(controller, model views, service)

--- GameStash/Controllers/ReviewController.cs
+++ GameStash/Controllers/ReviewController.cs
@@ -1,132 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
 using System.Linq;
-using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using GS_Data;
 
 namespace GameStash.Controllers
 {
     public class ReviewController : Controller
     {
-        private readonly ApplicationDbContext _db = new ApplicationDbContext();
-
         // GET: Review
         public ActionResult Index()
         {
-            var reviews = _db.Reviews.Include(r => r.Game);
-            return View(reviews.ToList());
-        }
-
-        // GET: Review/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Review review = _db.Reviews.Find(id);
-            if (review == null)
-            {
-                return HttpNotFound();
-            }
-            return View(review);
-        }
-
-        // GET: Review/Create
-        public ActionResult Create()
-        {
-            ViewBag.GameID = new SelectList(_db.Games, "GameID", "GameTitle");
             return View();
-        }
-
-        // POST: Review/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ReviewID,GameID,ReviewRating,ReviewDescription")] Review review)
-        {
-            if (ModelState.IsValid)
-            {
-                _db.Reviews.Add(review);
-                _db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.GameID = new SelectList(_db.Games, "GameID", "GameTitle", review.GameID);
-            return View(review);
-        }
-
-        // GET: Review/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Review review = _db.Reviews.Find(id);
-            if (review == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.GameID = new SelectList(_db.Games, "GameID", "GameTitle", review.GameID);
-            return View(review);
-        }
-
-        // POST: Review/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ReviewID,GameID,ReviewRating,ReviewDescription")] Review review)
-        {
-            if (ModelState.IsValid)
-            {
-                _db.Entry(review).State = EntityState.Modified;
-                _db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.GameID = new SelectList(_db.Games, "GameID", "GameTitle", review.GameID);
-            return View(review);
-        }
-
-        // GET: Review/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Review review = _db.Reviews.Find(id);
-            if (review == null)
-            {
-                return HttpNotFound();
-            }
-            return View(review);
-        }
-
-        // POST: Review/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Review review = _db.Reviews.Find(id);
-            _db.Reviews.Remove(review);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                _db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }

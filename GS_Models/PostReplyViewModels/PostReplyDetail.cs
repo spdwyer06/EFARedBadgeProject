f071d36679271f2caed7bb0d72af8b1c75b91d10
Sean Dwyergmail.com <spdwyer06@gmail.com>
2020-04-13 10:06:36 -0400

    Added PostReply view models

---
+++ GS_Models/PostReplyViewModels/PostReplyDetail.cs
@@ -0,0 +1,23 @@
+﻿using GS_Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GS_Models.PostReplyViewModels
+{
+    public class PostReplyDetail
+    {
+        public int ReplyID { get; set; }
+
+        public int PostID { get; set; }
+        public virtual Post Post { get; set; }
+
+        public Guid ReplyCreator { get; set; }
+
+        public string ReplyContent { get; set; }
+
+        public DateTimeOffset ReplyCreated { get; set; }
+    }
+}
---
+++ GS_Models/PostReplyViewModels/PostReplyEdit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GS_Models.PostReplyViewModels
+{
+    public class PostReplyEdit
+    {
+        public int ReplyID { get; set; }
+
+        public string ReplyContent { get; set; }
+    }
+}

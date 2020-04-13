8d805a061791056039ecc8e27d410788d3e04e50
Sean Dwyergmail.com <spdwyer06@gmail.com>
2020-04-12 23:49:50 -0400

    Thread view models added

---
+++ GS_Models/ThreadViewModels/ThreadCreate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GS_Models.ThreadViewModels
+{
+    public class ThreadCreate
+    {
+        public string ThreadTitle { get; set; }
+
+        public string ThreadDescription { get; set; }
+    }
+}

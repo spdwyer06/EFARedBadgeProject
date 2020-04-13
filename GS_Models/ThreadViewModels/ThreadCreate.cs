0063be45e196a9c4ea1ef34038c67f8b1a1215c6
Sean Dwyergmail.com <spdwyer06@gmail.com>
2020-04-13 12:42:32 -0400

    Added annotations to all Thread view models

--- GS_Models/ThreadViewModels/ThreadCreate.cs
+++ GS_Models/ThreadViewModels/ThreadCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace GS_Models.ThreadViewModels
 {
     public class ThreadCreate
     {
+        [Required, Display(Name = "Title")]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
         public string ThreadTitle { get; set; }
 
+        [Display(Name = "Description")]
+        [MaxLength(8000, ErrorMessage = "There are too many characters in this field.")]
         public string ThreadDescription { get; set; }
     }
 }

a1dcbb325331f413af66bb44ca4993c7f668db34
Sean Dwyergmail.com <spdwyer06@gmail.com>
2020-04-14 00:43:34 -0400

    Starting point for Game seed data created

--- GS_Data/Migrations/Configuration.cs
+++ GS_Data/Migrations/Configuration.cs
@@ -5,19 +5,25 @@
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<GS_Data.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(GS_Data.ApplicationDbContext context)
+        protected override void Seed(ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+
+            //context.Games.AddOrUpdate(
+            //    game => new { game.GameTitle, game.PlatformType },
+            //    new Game { OwnerId = Guid.Parse("00000000-0000-0000-0000-000000000000"), PlatformType = Game.TypeOfPlatform.Playstation4, CategoryType = Game.TypeOfCategory.FPS, RatingType = Game.TypeOfRating.M, GameTitle = "Doom", Price = 20 },
+            //    new Game { OwnerId = Guid.Parse("00000000-0000-0000-0000-000000000000"), PlatformType = Game.TypeOfPlatform.Playstation4, CategoryType = Game.TypeOfCategory.FPS, RatingType = Game.TypeOfRating.M, GameTitle = "Doom Eternal", Price = 60 }
+            //    );
         }
     }
 }
